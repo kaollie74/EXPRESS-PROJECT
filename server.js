9b96fa7c46b932014161cd0b71977bf4eadcaf9c
Kyle Olson <kaollie74@gmail.com>
2022-06-04 14:14:00 -0500

    set nodemon as dev dependency

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send("Hello")
-})
+const friends = [
+  { id: 0, name: "Kyle Olson" },
+  { id: 1, name: "Josh Terwey" },
+];
+
+app.get("/", (req, res) => {
+  res.send(friends);
+});
+
+app.get("/friend/:id", (req, res) => {
+  const id = friends[req.params.id];
+  if (id) {
+    console.log("inside if")
+    res.send(id);
+  } else {
+    console.log("inside else:")
+    res.status(404).json({ message: "Friend Does not exist" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`SERVER LISTENING ON ${PORT}....`);
